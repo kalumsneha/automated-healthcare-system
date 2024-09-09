f7d8b1c410f820f435f9ca1f885f8ac6385ab390
mikalumfernando <kalumsneha@yahoo.com>
2024-09-09 18:11:26 +0100

    Initial Commit

---
+++ src/main/java/com/titans/ahs/AutomatedHealthcareSystemApplication.java
@@ -0,0 +1,13 @@
+package com.titans.ahs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomatedHealthcareSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomatedHealthcareSystemApplication.class, args);
+	}
+
+}
